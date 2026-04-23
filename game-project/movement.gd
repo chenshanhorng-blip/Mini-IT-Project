a06011c92bf77e86ad5ecc39d67a1c0839e8738d
Teng Jia Yi <vijoy1129@gmail.com>
2026-04-24 00:20:06 +0800

    ✨ Add Godot game project  📌 Initial commit for Sharon-feature branch
    🎮 New Game Project!
    
    ⚡️ Added movement.gd
    💾 Basic setup complete

---
+++ game-project/movement.gd
@@ -0,0 +1,10 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("游戏开始了！")  # 添加这行
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# 添加一个简单的移动示例（如果你有角色节点的话）
+	pass
