35311ab0e0ca9da076117f8e5909ef542b4d25bc
Teng Jia Yi <vijoy1129@gmail.com>
2026-05-08 01:14:30 +0800

    basic button map and level system
    doing basic control button for map connect to level 1/2/3/4/5. Already done connecting to level1 and 2.

---
+++ game-project/script_level_map/map.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+func _ready():
+	print("Map script loaded")
+	$TextureRect.pressed.connect(_on_level_1_pressed)
+	$TextureRect2.pressed.connect(_on_level_2_pressed)
+	$TextureRect3.pressed.connect(_on_level_3_pressed)
+	$TextureRect4.pressed.connect(_on_level_4_pressed)
+	$TextureRect5.pressed.connect(_on_level_5_pressed)
+
+func _on_level_1_pressed():
+	print("Button clicked!")
+	get_tree().change_scene_to_file("res://scene_level_map/level1.tscn")
+
+func _on_level_2_pressed():
+	print("Button clicked!")
+	get_tree().change_scene_to_file("res://scene_level_map/level2.tscn")
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file(" ")
+
+func _on_level_4_pressed():
+	get_tree().change_scene_to_file(" ")
+
+func _on_level_5_pressed():
+	get_tree().change_scene_to_file(" ")
