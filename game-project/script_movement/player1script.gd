9c47c028e1ff1eb0b167dc6e83042bb794f2a8ca
Teng Jia Yi <vijoy1129@gmail.com>
2026-05-17 22:35:26 +0800

    character animation and level 1 system
    adding character animation inside level 1 and complete level 1 system which is touching three red point and avoid trap and also enermy

--- game-project/script_movement/player1script.gd
+++ game-project/script_movement/player1script.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
 @export var speed = 300.0
-
-@onready var sprite = $Sprite2D
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	var direction = Input.get_vector("p1_left", "p1_right", "p1_up", "p1_down")
 	velocity = direction * speed
-	
-	if direction.x !=0:
-		sprite.flip_h = direction.x<0
-	
 	move_and_slide()
+	
+	if direction.length() > 0:
+		if direction.x > 0:
+			animated_sprite.play("right_move")
+		elif direction.x < 0:
+			animated_sprite.play("left_move")
+		elif direction.y > 0:
+			animated_sprite.play("down")
+		elif direction.y < 0:
+			animated_sprite.play("jump") 
+	else:
+		animated_sprite.play("idle")
