5b57ad51d283046390f110e08c3c7b62b710132a
Teng Jia Yi <vijoy1129@gmail.com>
2026-06-02 22:25:43 +0800

    level 3 design and trap
    designing level 3 background, ground, and two trap which is fairy ring and vine trap

---
+++ game-project/TrapHazard_script/fairy_ring.gd
@@ -0,0 +1,35 @@
+# FairyRing.gd
+extends Area2D
+
+# 在编辑器里拖入其他传送目的地节点
+@export var teleport_destinations: Array[Node2D] = []
+@export var cooldown_time: float = 1.5  # 防止来回传送
+
+var can_teleport = true
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body.is_in_group("player") and can_teleport:
+		if teleport_destinations.is_empty():
+			return
+		
+		# 随机选一个目的地
+		var destination = teleport_destinations.pick_random()
+		
+		_teleport_player(body, destination.global_position)
+
+func _teleport_player(player, target_pos):
+	can_teleport = false
+	
+	# 播放特效和音效
+	$GPUParticles2D.emitting = true
+	$AudioStreamPlayer2D.play()
+	
+	# 传送玩家
+	player.global_position = target_pos
+	
+	# 冷却时间防止循环传送
+	await get_tree().create_timer(cooldown_time).timeout
+	can_teleport = true
