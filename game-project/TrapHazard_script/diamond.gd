db1160bc1d1ff0569413e7a470b9b76edd6b82b3
Teng Jia Yi <vijoy1129@gmail.com>
2026-06-10 01:51:14 +0800

    level 3 design complete

--- game-project/TrapHazard_script/diamond.gd
+++ game-project/TrapHazard_script/diamond.gd
@@ -3,9 +3,14 @@ extends Area2D
 signal collected
 
 func _ready():
+	monitoring = true
 	body_entered.connect(_on_body_entered)
+	print("Diamond ready: ", name)
+	print("Diamond monitoring: ", monitoring)
+	print("Diamond collision mask: ", collision_mask)
 
 func _on_body_entered(body):
-	if body.name == "CharacterBody2D":  
+	print("Body entered diamond: ", body.name)
+	if body is CharacterBody2D:
 		collected.emit()
 		queue_free()
